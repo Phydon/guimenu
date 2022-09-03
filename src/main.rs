5fc88af382d69cda5b49617141b26c2c5b8ada4c
Phydon <leann.phydon@gmail.com>
2022-09-03 15:10:06 +0200

    scrollable, added space, centered quitting confirmation window

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,83 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-use eframe::egui;
+use eframe::{egui, epaint::Vec2};
 
-fn main() {
-    let options = eframe::NativeOptions::default();
-    eframe::run_native(
-        "My egui App",
-        options,
-        Box::new(|_cc| Box::new(MyApp::default())),
-    );
-}
+const WINDOW_HEIGHT: f32 = 540.0;
+const WINDOW_WIDTH: f32 = 960.0;
+const CENTER: (f32, f32) = (WINDOW_HEIGHT / 2.0, WINDOW_WIDTH / 2.0);
+const PADDING: f32 = 20.0;
 
-struct MyApp {
+#[derive(Default)]
+struct GuiMenu {
     name: String,
-    age: u32,
+    number: u32,
+    result: u32,
+    allowed_to_close: bool,
+    show_confirmation_dialog: bool,
 }
 
-impl Default for MyApp {
-    fn default() -> Self {
-        Self {
-            name: "Arthur".to_owned(),
-            age: 42,
-        }
+impl eframe::App for GuiMenu {
+    fn on_close_event(&mut self) -> bool {
+        self.show_confirmation_dialog = true;
+        self.allowed_to_close
     }
-}
 
-impl eframe::App for MyApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("My egui Application");
-            ui.horizontal(|ui| {
-                ui.label("Your name: ");
-                ui.text_edit_singleline(&mut self.name);
+            ui.heading("THE Menu Application");
+            ui.add_space(PADDING);
+
+            egui::containers::ScrollArea::vertical().show(ui, |ui| {
+                ui.horizontal(|ui| {
+                    ui.label("Enter name: ");
+                    ui.text_edit_singleline(&mut self.name);
+                });
+                ui.add(egui::Slider::new(&mut self.number, 0..=120).text("number"));
+                if ui.button("Calculate").clicked() {
+                    self.result = calculate_it(self.number);
+                }
+                ui.label(format!("Name: '{}', result {}", self.name, self.result));
+
+                ui.add_space(PADDING);
+
+                for i in 1..=60 {
+                    ui.label(format!("just some txt {}", i as usize));
+                }
             });
-            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-            if ui.button("Click each year").clicked() {
-                self.age += 1;
+
+            if self.show_confirmation_dialog {
+            // Show confirmation dialog:
+            egui::Window::new("Do you want to quit?")
+                .collapsible(false)
+                .resizable(false)
+                .default_pos(CENTER)
+                .show(ctx, |ui| {
+                    ui.horizontal(|ui| {
+                        if ui.button("Cancel").clicked() {
+                            self.show_confirmation_dialog = false;
+                        }
+
+                        if ui.button("Yes!").clicked() {
+                            self.allowed_to_close = true;
+                            frame.close();
+                        }
+                    });
+                });
             }
-            ui.label(format!("Hello '{}', age {}", self.name, self.age));
         });
     }
+}
+
+fn calculate_it(num: u32) -> u32 {
+    num * num
+}
+
+fn main() {
+    let mut options = eframe::NativeOptions::default();
+    options.initial_window_size = Some(Vec2::new(WINDOW_HEIGHT, WINDOW_WIDTH));
+    eframe::run_native(
+        "GUI Menu",
+        options,
+        Box::new(|_cc| Box::new(GuiMenu::default())),
+    );
 }
