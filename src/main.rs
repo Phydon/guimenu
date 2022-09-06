55008364f28add5f4b323c7a51da96586b14cd72
Phydon <leann.phydon@gmail.com>
2022-09-06 12:41:20 +0200

    some polishing; reset button

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@
 
 use eframe::egui;
 
-const WINDOW_HEIGHT: f32 = 320.0;
-const WINDOW_WIDTH: f32 = 480.0;
-const CENTER: (f32, f32) = (WINDOW_HEIGHT * 0.5, WINDOW_WIDTH * 0.5);
+const WINDOW_HEIGHT: f32 = 480.0;
+const WINDOW_WIDTH: f32 = 960.0;
+const CENTER: (f32, f32) = (
+    (WINDOW_WIDTH - WINDOW_WIDTH * 0.60),
+    (WINDOW_HEIGHT - WINDOW_HEIGHT * 0.5),
+);
 const PADDING: f32 = 20.0;
 
-#[derive(Default)]
+#[derive(Default, PartialEq)]
 struct GuiMenu {
     name: String,
     number: u32,
@@ -23,33 +26,71 @@ impl eframe::App for GuiMenu {
     }
 
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        // for the reset button
+        let Self {
+            name: _,
+            number: _,
+            result: _,
+            allowed_to_close: _,
+            show_confirmation_dialog: _,
+        } = self;
+
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            ui.vertical_centered(|ui| {
-                ui.heading("Hello World! From `TopBottomPanel`, that must be before `CentralPanel`!");
+            ui.add_space(2.0);
+            egui::menu::bar(ui, |ui| {
+                ui.with_layout(egui::Layout::left_to_right(egui::Align::LEFT), |ui| {
+                    ui.collapsing("Themes", |ui| {
+                        egui::widgets::global_dark_light_mode_buttons(ui);
+                    });
+                });
+
+                ui.with_layout(egui::Layout::right_to_left(egui::Align::RIGHT), |ui| {
+                    egui::reset_button(ui, self);
+                });
+            
             });
+            ui.add_space(2.0);
+        });
+
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            ui.add_space(8.0);
+            ui.vertical_centered(|ui| {
+                ui.label(egui::RichText::new("GUIMENU").size(40.0).strong().color(egui::Color32::from_rgb(171, 39, 79)));
+            });
+            ui.add_space(8.0);
         });
 
         egui::TopBottomPanel::bottom("bottom_panel").show(ctx, |ui| {
+            ui.add_space(2.0);
             ui.vertical_centered(|ui| {
                 let tooltip_text = "This is an email adress";
-                ui.hyperlink("wasd.qwertz@email.com").on_hover_text(tooltip_text);
+                ui.hyperlink("wasd.qwertz@email.com")
+                    .on_hover_text(tooltip_text);
             });
+            ui.add_space(2.0);
         });
 
         egui::SidePanel::left("left_panel").show(ctx, |ui| {
+            ui.add_space(2.0);
             egui::containers::ScrollArea::vertical().show(ui, |ui| {
                 for i in 1..=60 {
                     ui.label(format!("just some txt {}", i as usize));
                 }
             });
+            ui.add_space(2.0);
         });
 
         egui::SidePanel::right("right_panel").show(ctx, |ui| {
-            egui::widgets::global_dark_light_mode_buttons(ui);
+            ui.add_space(2.0);
+            egui::containers::ScrollArea::vertical().show(ui, |ui| {
+                ui.label("Right");
+            });
+            ui.add_space(2.0);
         });
 
         // CentralPanel must be added after all other panels!
         egui::CentralPanel::default().show(ctx, |ui| {
+            ui.add_space(2.0);
             ui.heading("THE Menu Application");
             ui.separator();
 
@@ -78,10 +119,15 @@ impl eframe::App for GuiMenu {
                 }
                 ui.add_space(PADDING);
                 ui.label(format!(
-                    "Name: '{}', result {}",
-                    self.name, self.result
+                    "Name: '{}'",
+                    self.name, 
                 ));
+                ui.label(egui::RichText::new(format!(
+                    "result {}",
+                    self.result
+                )).size(25.0).italics());
             });
+            ui.add_space(2.0);
 
             if self.show_confirmation_dialog {
                 // Show confirmation dialog:
